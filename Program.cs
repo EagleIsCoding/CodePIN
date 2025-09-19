be0b163e8fe5bf6e9aaf4b121d11a500ff63df9a
Eagle <80061096+EagleIsCoding@users.noreply.github.com>
2025-09-19 13:54:47 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace CasserCodePIN
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // start représente l'heure de démarrage du programme
+            DateTime start = DateTime.Now;
+            Console.WriteLine("Début du programme");
+            Random rnd = new Random();
+            int codeSecret = rnd.Next(9999);
+            // booléen qui indique si on a trouvé le code secret
+            bool trouve = false;
+            // compteur de boucle
+            int i = 0;
+            while (i < 10000 && trouve == false)
+            {
+                Console.WriteLine(i + " ");
+                // si on trouve le code pin, il faut afficher i et interrompre la boucle
+                if (i == codeSecret)
+                {
+                    Console.WriteLine("Code PIN trouvé : " + i);
+                    trouve = true;
+                }
+                Thread.Sleep(1000);
+                i++;
+            }
+            Console.WriteLine("Fin du programme");
+            // durée du programme = heure de fin - heure de démarrage
+            TimeSpan duree = DateTime.Now - start;
+            Console.WriteLine("durée du programme : " + duree);
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+# 🔐 Casser un Code PIN 
+
+---
+
+### ✅ **# Quelle est la durée de ce programme en secondes ? En millisecondes (ms) ?**
+
+* **Durée en secondes** : `0,0147277 s`
+* **Durée en millisecondes** : `14,73 ms`
+
+### 🔎 **Résultat**
+
+![Résultat](./resultat.png)
+
+---
+
+### ⌛ **Quelle est la durée du programme en secondes ?**
+
+* **0,0592141 secondes**
+
+### 🔁 **Relancez le programme avec temporisation de 5ms. Quelle est sa durée ?**
+
+* **38 secondes**
+
+### ⏳ **Mettez maintenant une temporisation de 1 seconde (= 1 000 ms). Lancez le programme : quel est l’effet sur l’attaquant ?**
+
+> L'effet pour l'attaquant est que **plus la temporisation est grande**, **plus il prendra de temps à trouver le code.** C'est dissuasif.
+
+---
